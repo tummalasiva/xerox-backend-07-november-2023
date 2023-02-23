d0767d3882297220fc3f677ff535c6a52111b4c5
rakeshSgr <rakeshkumar@rakeshs-MacBook-Air.local>
2023-02-23 13:15:29 +0530

    apis fixes

--- src/envVariables.js
+++ src/envVariables.js
@@ -27,49 +27,9 @@ let enviromentVariables = {
 		message: 'Required refresh token secret',
 		optional: false,
 	},
-	APP_NAME: {
-		message: 'Application Name',
-		optional: false,
-	},
-	REGISTRATION_EMAIL_TEMPLATE_CODE: {
-		message: 'Required registration email template code',
-		optional: false,
-	},
-	OTP_EMAIL_TEMPLATE_CODE: {
-		message: 'Required otp email template code',
-		optional: false,
-	},
-	KAFKA_URL: {
-		message: 'Required kafka connectivity url',
-		optional: false,
-	},
-	KAFKA_GROUP_ID: {
-		message: 'Required kafka group id',
-		optional: false,
-	},
-	KAFKA_TOPIC: {
-		message: 'Required kafka topic to consume from',
-		optional: true,
-	},
-	NOTIFICATION_KAFKA_TOPIC: {
-		message: 'Required kafka topic',
-		optional: false,
-	},
 	CLOUD_STORAGE: {
 		message: 'Required cloud storage type ex: AWS/GCP/AZURE',
 		optional: false,
-	},
-	GCP_PATH: {
-		message: 'Required gcp file path ex: gcp.json',
-		optional: process.env.CLOUD_STORAGE === 'GCP' ? false : true,
-	},
-	DEFAULT_GCP_BUCKET_NAME: {
-		message: 'Required gcp bucket name',
-		optional: process.env.CLOUD_STORAGE === 'GCP' ? false : true,
-	},
-	GCP_PROJECT_ID: {
-		message: 'Required gcp project id',
-		optional: process.env.CLOUD_STORAGE === 'GCP' ? false : true,
 	},
 	AWS_ACCESS_KEY_ID: {
 		message: 'Required aws access key id',
@@ -91,22 +51,6 @@ let enviromentVariables = {
 		message: 'Required aws bucket name',
 		optional: process.env.CLOUD_STORAGE === 'AWS' ? false : true,
 	},
-	AZURE_ACCOUNT_NAME: {
-		message: 'Required azure account name',
-		optional: process.env.CLOUD_STORAGE === 'AZURE' ? false : true,
-	},
-	AZURE_ACCOUNT_KEY: {
-		message: 'Required azure account key',
-		optional: process.env.CLOUD_STORAGE === 'AZURE' ? false : true,
-	},
-	DEFAULT_AZURE_CONTAINER_NAME: {
-		message: 'Required azure container name',
-		optional: process.env.CLOUD_STORAGE === 'AZURE' ? false : true,
-	},
-	MENTOR_SECRET_CODE: {
-		message: 'Required mentor secret code',
-		optional: false,
-	},
 	ACCESS_TOKEN_EXPIRY: {
 		message: 'Required access token expiry in days',
 		optional: false,
@@ -123,11 +67,6 @@ let enviromentVariables = {
 		message: 'Internal Cache Expiry Time',
 		optional: false,
 	},
-
-	RATING_KAFKA_TOPIC: {
-		message: 'Kafka Rating Topic',
-		optional: false,
-	},
 	REDIS_HOST: {
 		message: 'Redis Host Url',
 		optional: false,
@@ -140,34 +79,6 @@ let enviromentVariables = {
 		message: 'iv is missing for email encryption',
 		optional: false,
 	},
-	OCI_ACCESS_KEY_ID: {
-		message: 'Required oci access key id',
-		optional: process.env.CLOUD_STORAGE === 'OCI' ? false : true,
-	},
-	OCI_SECRET_ACCESS_KEY: {
-		message: 'Required oci secret access key',
-		optional: process.env.CLOUD_STORAGE === 'OCI' ? false : true,
-	},
-	OCI_BUCKET_REGION: {
-		message: 'Required oci bucket region',
-		optional: process.env.CLOUD_STORAGE === 'OCI' ? false : true,
-	},
-	OCI_BUCKET_ENDPOINT: {
-		message: 'Required oci bucket endpoint',
-		optional: process.env.CLOUD_STORAGE === 'OCI' ? false : true,
-	},
-	DEFAULT_OCI_BUCKET_NAME: {
-		message: 'Required oci bucket name',
-		optional: process.env.CLOUD_STORAGE === 'OCI' ? false : true,
-	},
-	REPLICA_SET_NAME: {
-		message: 'Required replica set name',
-		optional: true,
-	},
-	REPLICA_SET_READ_PREFERENCE: {
-		message: 'Required replica read preferance',
-		optional: process.env.REPLICA_SET_NAME ? false : true,
-	},
 	ERROR_LOG_LEVEL: {
 		message: 'Required Error log level',
 		optional: false,
@@ -175,11 +86,7 @@ let enviromentVariables = {
 	DISABLE_LOG: {
 		message: 'Required disable log level',
 		optional: false,
-	},
-	DEFAULT_ORGANISATION_CODE: {
-		message: 'Required default organisation code ',
-		optional: false,
-	},
+	}
 }
 
 let success = true
