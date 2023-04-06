6c79cee827b8aabdf4dc2d249b235a8057edc9c3
rakeshSgr <rakeshkumar@PW110.local>
2023-04-06 16:57:04 +0530

    fixe

--- src/controllers/v1/payment.js
+++ src/controllers/v1/payment.js
@@ -14,4 +14,20 @@ module.exports = class Payment {
         return error;
         }
     }
+
+    async confirm(req) {
+        const params = req.body;
+        try {
+
+            console.log("======================");
+            console.log("======================",params);
+            
+            console.log("======================");
+            
+        // const result = await paymentHelper.order(params,req.decodedToken._id);
+        return result;
+        } catch (error) {
+        return error;
+        }
+    }
 }
